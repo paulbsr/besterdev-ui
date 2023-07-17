2efbaa9aca02463f751571a7110732f106529b4c
Paul Bester <paul.bester@outlook.com>
2023-07-17 19:32:00 +0100

    Great Progress, Bester

--- src/CandidateAmend.jsx
+++ src/CandidateAmend.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 import 'react-dropdown/style.css';
 import {FaPen, FaCheck, FaRegTrashAlt} from 'react-icons/fa';
 import {PiArrowCounterClockwiseBold} from 'react-icons/pi';
-import { GiHummingbird, GiNestBirds, GiKiwiBird } from "react-icons/gi";
+import { GiKiwiBird } from "react-icons/gi";
 import AlertContext from './Generic/Alerts/AlertContext';
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -30,6 +30,10 @@ export default function CandidateAmend(props) {
   const [email, setemail] = useState(null)
   const [mobile, setmobile] = useState(null)
   const [dob, setdob] = useState(null)
+  const [jobdesc, setjobdesc] = useState(null)
+  const [skill1, setskill1] = useState(null)
+  const [comment, setcomment] = useState(null)
+  const [status, setstatus] = useState(null)
   const [cr_datehold, setCr_DateHold] = useState(null)
   const [crDate, setCrDate] = useState(null)
   const alertCtx = useContext(AlertContext);
@@ -46,6 +50,10 @@ export default function CandidateAmend(props) {
           setemail(row.email)
           setmobile(row.mobile)
           setdob(row.dob)
+          setjobdesc(row.jobdesc)
+          setskill1(row.skill1)
+          setcomment(row.comment)
+          setstatus(row.status)
         };
 
         const onEditCancel = () => {
@@ -55,6 +63,10 @@ export default function CandidateAmend(props) {
           setemail(null)
           setmobile(null)
           setdob(null)
+          setjobdesc(null)
+          setskill1(null)
+          setcomment(null)
+          setstatus(null)
         };
 
         const handleDateChange = (newVal) => {
@@ -65,24 +77,28 @@ export default function CandidateAmend(props) {
         const onEditSave = async() => {
         { 
             
-        const recordPUT = {
+        const candidatePUT = {
           "firstname": firstname,
           "lastname": lastname,
           "email": email,
           "mobile": mobile,
           "dob": crDate,
-           } 
+          "jobdesc": jobdesc,
+          "skill1":skill1,
+          "comment":comment,
+          "status":status
+        } 
            
                             
-           await axios.put(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/records/update/${editing}`, recordPUT)
-            .then((response) => {props.setCheckForRecords(!props.checkForRecords); alertCtx.success(`Suksesvolle PUT`); })
+           await axios.put(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/candidates/update/${editing}`, candidatePUT)
+            .then((response) => {props.setCheckForRecords(!props.checkForRecords); console.log(candidatePUT); alertCtx.success(`Suksesvolle PUT`); })
             .catch((error) => {alertCtx.error(error.message);})
             setCheckForRecords(!props.checkForRecords)
             onEditCancel();}
             }
 
           const onEditDelete = (row) => {
-            axios.delete(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/records/delete/${row.id}`)
+            axios.delete(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/candidates/delete/${row.id}`)
             .then((response) => {props.setCheckForRecords(!props.checkForRecords); alertCtx.success(`Suksesvolle DEL`)})
             
             };       
@@ -108,12 +124,16 @@ export default function CandidateAmend(props) {
             <table className="Table6">
               <thead>
                 <tr>
-                  <th align='center'></th>
-                  <th style={{ width: '150px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Firstname</th>
-                  <th style={{ width: '200px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Lastname</th>
-                  <th style={{ width: '350px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>eMail Address</th>
-                  <th style={{ width: '200px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Mobile/Cell</th>
-                  <th style={{ width: '100px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Date Found</th>
+                  <th style={{ width: '20px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'></th>
+                  <th style={{ width: '110px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Firstname</th>
+                  <th style={{ width: '150px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Lastname</th>
+                  <th style={{ width: '300px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>eMail</th>
+                  <th style={{ width: '150px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Mobile</th>
+                  <th style={{ width: '100px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Date Found</th>
+                  <th style={{ width: '200px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Job Title</th>
+                  <th style={{ width: '200px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Skilled</th>
+                  <th style={{ width: '500px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Comment</th>
+                  <th style={{ width: '50px', borderRadius: '4px' }} className="Font-Verdana-Small_Compliment_Blue" align='center'>Status</th>
                 </tr>
               </thead>
 
@@ -134,16 +154,16 @@ export default function CandidateAmend(props) {
                             )
                             :
                             (
-                              <button style={{ height: '20px', width: '20px', padding: 0, border: 'none', borderRadius: '3px', backgroundColor: '#1994AD', outline: 'none' }} type='button' onClick={() => handleEdit(row)}><a data-tooltip-id="edit" data-tooltip-content="Edit"><FaPen style={{ color: 'white', display: 'block', margin: 'auto', fontSize: '12px', cursor: 'pointer' }} /></a></button>
+                                <button style={{ height: '20px', width: '20px', padding: 0, border: 'none', borderRadius: '3px', backgroundColor: '#1994AD', outline: 'none' }} type='button' onClick={() => handleEdit(row)}><a data-tooltip-id="edit" data-tooltip-content="Edit"><FaPen style={{ color: 'white', display: 'block', margin: 'auto', fontSize: '12px', cursor: 'pointer' }} /></a></button>
                             )
                           }
                         </>
                       </td>
 
-                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '380px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px' }} value={firstname} onChange={(e) => setfirstname(e.target.value)} className='cr_edit_inputfield' />) : (<a href={'www.dell.com' + (row.firstname) + '"+&action=&title=' + (row.firstname)} target="_blank">{row.firstname}</a>)}</td>
-                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '380px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px' }} value={lastname} onChange={(e) => setlastname(e.target.value)} className='cr_edit_inputfield' />) : (<a href={'https://dashboard.heroku.com/apps'} target="_blank">{row.lastname}</a>)}</td>
-                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '380px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px' }} value={email} onChange={(e) => setemail(e.target.value)} className='cr_edit_inputfield_disc' />) : (row.email)}</td>
-                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '380px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px' }} value={mobile} onChange={(e) => setmobile(e.target.value)} className='cr_edit_inputfield' />) : (row.mobile)}</td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '90px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={firstname} onChange={(e) => setfirstname(e.target.value)} className='cr_edit_inputfield' />) : (<a href={'www.dell.com' + (row.firstname) + '"+&action=&title=' + (row.firstname)} target="_blank">{row.firstname}</a>)}</td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '130px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={lastname} onChange={(e) => setlastname(e.target.value)} className='cr_edit_inputfield' />) : (<a href={'https://dashboard.heroku.com/apps'} target="_blank">{row.lastname}</a>)}</td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '280px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={email} onChange={(e) => setemail(e.target.value)} className='cr_edit_inputfield_disc' />) : (row.email)}</td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '130px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={mobile} onChange={(e) => setmobile(e.target.value)} className='cr_edit_inputfield' />) : (row.mobile)}</td>
                       <td className="asmshover Table6 td">{row.id === editing ? (<LocalizationProvider dateAdapter={AdapterDayjs} dateLibInstance={dayjs.utc}>
                         <DatePicker
                           id="cr_date"
@@ -152,10 +172,14 @@ export default function CandidateAmend(props) {
                           selected={dob}
                           onChange={handleDateChange}
                           dateFormat="YYYY.M.D"
-                          sx={{ height: '22.5px', '& .MuiInputBase-root': { height: '100%', fontSize: '13.5px', width: '150px' }, '& .MuiSvgIcon-root': { height: '20px' } }}
+                          sx={{ height: '22.5px', '& .MuiInputBase-root': { height: '100%', fontSize: '13.5px', width: '80px' }, '& .MuiSvgIcon-root': { height: '20px' } }}
                         />
                       </LocalizationProvider>) : new Date(row.dob).toLocaleDateString("en-CA")}
                       </td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '180px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={jobdesc} onChange={(e) => setjobdesc(e.target.value)} className='cr_edit_inputfield' />) : (row.jobdesc)}</td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '180px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={skill1} onChange={(e) => setskill1(e.target.value)} className='cr_edit_inputfield' />) : (row.skill1)}</td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '480px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={comment} onChange={(e) => setcomment(e.target.value)} className='cr_edit_inputfield' />) : (row.comment)}</td>
+                      <td className="asmshover Table6 td">{row.id === editing ? (<input style={{ height: '22.5px', width: '30px', border: '1.25px solid #1994AD', borderRadius: '4px', padding: 0, paddingLeft: '5px' }} value={status} onChange={(e) => setstatus(e.target.value)} className='cr_edit_inputfield' />) : (row.status)}</td>
                     </tr>
                   )
                 })
--- src/CandidateScreen.jsx
+++ src/CandidateScreen.jsx
@@ -2,118 +2,110 @@ import { useState, useEffect, useContext } from 'react'
 import axios from 'axios'
 import AlertContext from "./Generic/Alerts/AlertContext";
 import './Fonts.css'
-import spacer from './graphix/besterdev_spacer_white.png'
-import {FaMinusCircle, FaPlusCircle} from 'react-icons/fa';
+import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa';
 import GradientLineThin from './GradientLineThin';
-import { GiHummingbird, GiNestBirds, GiBirdCage } from "react-icons/gi";
+import { GiBirdCage } from "react-icons/gi";
 import 'react-tooltip/dist/react-tooltip.css'
 import { Tooltip } from 'react-tooltip'
 
-
-
 export default function CandidateScreen() {
 
-    const [candidatedata, setCandidatedata] = useState([]);
-    const [firstname, setFirstname] = useState(null)
-    const [lastname, setLastname] = useState(null)
-    const [mobile, setMobile] = useState(null)
-    const [email, setEmail] = useState(null)
-    const [country, setCountry] = useState(null)
-    const alertCtx = useContext(AlertContext);
-    const [isExpanded, setExpanded] = useState(false);
-    const toggleAccordion = () => {setExpanded(!isExpanded);};
+  const [candidatedata, setCandidatedata] = useState([]);
+  const [firstname, setFirstname] = useState(null)
+  const [lastname, setLastname] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [mobile, setMobile] = useState(null)
+  const [dob, setDob] = useState(null)
+  const [jobdesc, setJobdesc] = useState(null)
+  const [skill1, setSkill1] = useState(null)
+  const [country, setCountry] = useState(null)
+  const alertCtx = useContext(AlertContext);
+  const [isExpanded, setExpanded] = useState(false);
+  const toggleAccordion = () => { setExpanded(!isExpanded); };
 
-    const onKeepPost = async (event) => {
-    
-        {
-            var candidatePOST = {
-                'firstname': firstname,
-                'lastname': lastname,
-                'mobile': mobile,
-                'email': email,
-                'country': country,
-            }
-            console.log(candidatePOST)
-            const response = await axios.post(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/candidates/create`, candidatePOST);
-            if (response.status === 200) { alert(`${firstname} ${lastname} has been saved for future reference`) }
-            else { alertCtx.error(`oops! Something went wrong!`); }
-        }
-    }
+  const onKeepPost = async (event) => {
 
-    useEffect(() => {
-        axios('https://randomuser.me/api/')
-            .then((response) => {
-                setCandidatedata(response.data.results);
-            })
-    },
-        []);
-
-
-    function GenderLabel({ gender }) {
-        if (gender === "female") { return <span>F</span>; }
-        else if (gender === "male") { return <span>M</span>; }
-        else { return null; }
-    }
- 
-
-    return (
-        <>
-        <GradientLineThin />&nbsp;
-        
-        
-
-
-        <div className='Font-Verdana-Small'>&nbsp; &nbsp;
-            <Tooltip id="insert" />
-            <div onClick={toggleAccordion}>
-                &nbsp;<a data-tooltip-id="insert" data-tooltip-content="Select"><GiBirdCage style={{ color: '#000000', fontSize: '25px', cursor: 'pointer' }} /></a>
-                &nbsp;<b>Screen Candidates</b>
-            </div>
-
-            {isExpanded && (
-                <div>
-                    <div>
-
-                        {
-        candidatedata.map((inbound, key) => {
-          return (
-            <form onSubmit={onKeepPost}>
-              <div key={key}>
-                <div className='Font-Verdana-Small'>
-                  <Tooltip id="Screen" />
-                  <div onClick={toggleAccordion}></div>
-                  {/* <a data-tooltip-id="Screen" data-tooltip-content="Screen"><GiNestBirds style={{ color: '#000000', fontSize: '35px', cursor: 'pointer' }} /></a>
-                  <b>&nbsp;Screen Candidate: </b> */}
-                </div>
-
-
-
-                <div className='Font-Verdana-Small'>
-                  &nbsp;
-                  <button style={{ height: '20px', width: '20px', padding: 0, border: 'none', borderRadius: '3px', backgroundColor: '#169247', outline: 'none'}} onClick={() => setFirstname(inbound.name.first) & setLastname(inbound.name.last) & setMobile(inbound.phone) & setEmail(inbound.email) & setCountry(inbound.location.country)}><a data-tooltip-id="commit" data-tooltip-content="Keep"><FaPlusCircle style={{ color: 'white', display: 'block', margin: 'auto', fontSize: '12px', cursor: 'pointer' }}/></a></button>
-                  &nbsp;
-                  <button style={{ height: '20px', width: '20px', padding: 0, border: 'none', borderRadius: '3px', backgroundColor: '#D5441C', outline: 'none'}} onClick={() => setFirstname(inbound.name.first) & setLastname(inbound.name.last) & setMobile(inbound.phone) & setEmail(inbound.email) & setCountry(inbound.location.country)}><a data-tooltip-id="commit" data-tooltip-content="Drop"><FaMinusCircle style={{ color: 'white', display: 'block', margin: 'auto', fontSize: '12px', cursor: 'pointer' }}/></a></button>
-                  &nbsp;
-                  <span style={{ color: 'black', fontStyle: 'bold'}}>{inbound.name.first} {inbound.name.last}</span> is a {inbound.dob.age}<GenderLabel gender={inbound.gender}/> <span style={{ color: 'red', fontStyle: 'italic'}}>{inbound.login.username}</span> who currently resides in {inbound.location.state}, {inbound.location.country} with primary skills of <span style={{ color: 'red', fontStyle: 'italic'}}>{inbound.login.username}</span> and <span style={{ color: 'red', fontStyle: 'italic'}}>{inbound.login.username}</span> and <span style={{ color: 'red', fontStyle: 'italic'}}>{inbound.login.username}</span> whom can be reached at {inbound.phone} or <a href={"mailto:${email}"}>{inbound.email}</a>
-                </div>
-
-              </div>
-            </form>
-          );
-        })
-        
+    {
+      var candidatePOST = {
+        'firstname': firstname,
+        'lastname': lastname,
+        'mobile': mobile,
+        'email': email,
+        'country': country,
+        'dob': "1999-09-09",
+        'jobdesc': jobdesc,
+        'skill1': skill1
       }
+      console.log(candidatePOST)
+      const response = await axios.post(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/candidates/create`, candidatePOST);
+      if (response.status === 200) { alert(`${firstname} ${lastname} has been saved for future reference`) }
+      else { alertCtx.error(`oops! Something went wrong!`); }
+    }
+  }
+
+  useEffect(() => {
+    axios('https://randomuser.me/api/')
+      .then((response) => {
+        setCandidatedata(response.data.results);
+      })
+  },
+    []);
+
+
+  function GenderLabel({ gender }) {
+    if (gender === "female") { return <span>F</span>; }
+    else if (gender === "male") { return <span>M</span>; }
+    else { return null; }
+  }
+
+
+  return (
+    <>
+      <GradientLineThin />&nbsp;
 
 
 
 
-
-
-
-
-                    </div>
-                </div>)}
+      <div className='Font-Verdana-Small'>&nbsp; &nbsp;
+        <Tooltip id="insert" />
+        <div onClick={toggleAccordion}>
+          &nbsp;<a data-tooltip-id="insert" data-tooltip-content="Select"><GiBirdCage style={{ color: '#000000', fontSize: '25px', cursor: 'pointer' }} /></a>
+          &nbsp;<b>Screen Candidates</b>
         </div>
-        </>
-    );
+
+        {isExpanded && (
+          <div>
+            <div>
+
+              {
+                candidatedata.map((inbound, key) => {
+                  return (
+                    <form onSubmit={onKeepPost}>
+                      <div key={key}>
+
+                        <div className='Font-Verdana-Small'>
+                          <Tooltip id="Screen" />
+                          <div onClick={toggleAccordion}></div>
+                        </div>
+
+                        <div className='Font-Verdana-Small'>
+                          &nbsp;
+                          <button style={{ height: '20px', width: '20px', padding: 0, border: 'none', borderRadius: '3px', backgroundColor: '#169247', outline: 'none' }} onClick={() => setFirstname(inbound.name.first) & setLastname(inbound.name.last) & setMobile(inbound.phone) & setEmail(inbound.email) & setCountry(inbound.location.country) & setJobdesc(inbound.location.coordinates.latitude) & setSkill1(inbound.location.coordinates.longitude)}><a data-tooltip-id="commit" data-tooltip-content="Keep"><FaPlusCircle style={{ color: 'white', display: 'block', margin: 'auto', fontSize: '12px', cursor: 'pointer' }} /></a></button>
+                          &nbsp;
+                          {/* <button style={{ height: '20px', width: '20px', padding: 0, border: 'none', borderRadius: '3px', backgroundColor: '#D5441C', outline: 'none' }} onClick={() => setFirstname(inbound.name.first) & setLastname(inbound.name.last) & setMobile(inbound.phone) & setEmail(inbound.email) & setCountry(inbound.location.country)}><a data-tooltip-id="commit" data-tooltip-content="Drop"><FaMinusCircle style={{ color: 'white', display: 'block', margin: 'auto', fontSize: '12px', cursor: 'pointer' }} /></a></button> */}
+                          &nbsp;
+                          <span style={{ color: 'black', fontStyle: 'bold' }}>{inbound.name.first} {inbound.name.last}</span> is a {inbound.dob.age}<GenderLabel gender={inbound.gender} /> <span style={{ color: 'red', fontStyle: 'italic' }}>{inbound.login.username}</span> who currently resides in {inbound.location.state}, {inbound.location.country} with primary skills of <span style={{ color: 'red', fontStyle: 'italic' }}>{inbound.login.username}</span> and <span style={{ color: 'red', fontStyle: 'italic' }}>{inbound.login.username}</span> and <span style={{ color: 'red', fontStyle: 'italic' }}>{inbound.login.username}</span> whom can be reached at {inbound.phone} or <a href={"mailto:${email}"}>{inbound.email}</a>
+                        </div>
+
+                      </div>
+                    </form>
+                  );
+                })
+
+              }
+            </div>
+          </div>)}
+      </div>
+    </>
+  );
 }
