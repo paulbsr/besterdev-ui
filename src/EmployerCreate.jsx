4295be3f60e14d70c0896c44c817425e5928a532
Paul Bester <paul.bester@outlook.com>
2023-08-08 16:53:43 +0100

    More

--- src/EmployerCreate.jsx
+++ src/EmployerCreate.jsx
@@ -80,13 +80,22 @@ export default function EmployerCreate(props) {
             <form onSubmit={handleSubmit}>
               <div><img alt="1" src={spacer2} /></div>
               <div className='Font-Verdana-Small-Postgres'>
-                <div><img alt="1" src={spacer} /><img alt="1" src={spacer} />Company Name:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '230px' }} placeholder="Required" type="text" value={empname} onChange={(event) => setEmpname(event.target.value)} required /><img alt="1" src={spacer} /><img alt="1" src={spacer} />&nbsp;Comment:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '675px' }} type="text" value={empcomment} onChange={(event) => setEmpcomment(event.target.value)} /></div>
+              <div>
+                <img alt="1" src={spacer} /><img alt="1" src={spacer} />Company Name:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '230px' }} placeholder="Required" type="text" value={empname} onChange={(event) => setEmpname(event.target.value)} required />
+                <img alt="1" src={spacer} /><img alt="1" src={spacer} />Comment:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '1130px' }} type="text" value={empcomment} onChange={(event) => setEmpcomment(event.target.value)} />
+              </div>
+
                 <div>&nbsp;</div>
-                <div><img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's First Name:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '200px' }} placeholder="Required" type="text" value={empcontactfn} onChange={(event) => setEmpcontactfn(event.target.value)} required />
-                  <img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's Last Name:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '160px' }} placeholder="Required" type="text" value={empcontactln} onChange={(event) => setEmpcontactln(event.target.value)} required />
-                  <img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's eMail Address:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '200px' }} placeholder="Required" type="text" value={empcontactemail} onChange={(event) => setEmpcontactemail(event.target.value)} required />
-                  <img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's Mobile Number:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '200px' }} placeholder="Required" type="text" value={empcontactnum} onChange={(event) => setEmpcontactnum(event.target.value)} required /></div>
+                
+              <div>
+                 <img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's First Name:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '200px' }} placeholder="Required" type="text" value={empcontactfn} onChange={(event) => setEmpcontactfn(event.target.value)} required />
+                 <img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's Last Name:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '160px' }} placeholder="Required" type="text" value={empcontactln} onChange={(event) => setEmpcontactln(event.target.value)} required />
+                 <img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's eMail Address:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '200px' }} placeholder="Required" type="text" value={empcontactemail} onChange={(event) => setEmpcontactemail(event.target.value)} required />
+                 <img alt="1" src={spacer} /><img alt="1" src={spacer} />Contact's Mobile Number:&nbsp;<input style={{ height: '27.5px', border: '1.25px solid #c4c4c4', borderRadius: '4px', padding: 0, paddingLeft: '10px', width: '200px' }} placeholder="Required" type="text" value={empcontactnum} onChange={(event) => setEmpcontactnum(event.target.value)} required />
+              </div>
+
                 <div>&nbsp;</div>
+
                 <div><img alt="1" src={spacer} />&nbsp; &nbsp; &nbsp;<button className="Font-Verdana-Small-Postgres" type="submit" style={{ marginLeft: '10px', height: '27.5px', border: '1px solid #336791', borderRadius: '5px', backgroundColor: '#FFFFFF', color: '#336791', cursor: 'pointer' }}>Add Employer/Customer</button></div>
               </div>
             </form>
