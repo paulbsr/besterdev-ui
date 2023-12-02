de113f3635428772ed49e95c6fec30dc31163e97
paulbsr <paul.bester@outlook.com>
2023-12-02 16:51:57 +0000

    Taskrecords works

--- src/TaskRecordAccordion.jsx
+++ src/TaskRecordAccordion.jsx
@@ -11,17 +11,18 @@ import { AiOutlineFileAdd, AiOutlineCheckCircle, AiOutlineEdit } from "react-ico
 function TaskRecordAccordion({ alertCtx, project_handle, asms_number, parentid, parenttask, checkForRecords, setCheckForRecords, taskstatus }) {
     const [isExpanded, setExpanded] = useState(false);
     const toggleAccordion = () => { setExpanded(!isExpanded); };
-    // const orderedTasks = parenttask.filter((task, key) => { return task.id === parentid });
     const orderedTasks = parenttask.filter((task, key) => { return task.id === parentid });
-    // const taskRecords = orderedTasks[0].tasks.sort((a, b) => new Date(b.date[0], b.date[1], b.date[2]) - new Date(a.date[0], a.date[1], a.date[2]) || b.childid - a.childid);
-    // const taskRecords = orderedTasks.sort((a, b) => b.childid - a.childid);
-    const taskRecords = orderedTasks;
+    // const orderedTasks = parenttask.filter((task, key) => { return task.id === parentid });
+    const taskRecords = orderedTasks[0].tasks.sort((a, b) => new Date(b.date[0], b.date[1], b.date[2]) - new Date(a.date[0], a.date[1], a.date[2]) || b.childid - a.childid);
+    // const taskRecords = orderedTasks.sort((a, b) => a.childid - b.childid);
+    // const taskRecords = orderedTasks;
     // const taskRecords = orderedTasks;
     const [editing, setEditing] = useState(false);
     const [taskrecord, setTaskrecord] = useState(null);
     const date = new Date();
-    console.log(parenttask)
-    console.log(orderedTasks)
+    console.log(parenttask) //so parenttask is good and contains the "tasks"
+    console.log(orderedTasks) 
+    console.log(taskRecords)
   
 
     const handleEdit = (id, childrecord) => {
@@ -94,22 +95,25 @@ function TaskRecordAccordion({ alertCtx, project_handle, asms_number, parentid, 
         )
     }
 
-    // if (taskRecords.length > 0) {
-    //     if (taskRecords[0].status === 'DONE') {
-    //         return (
-    //             <div>
-    //                 <TaskRecordStatusByColourLong alertCtx={alertCtx} childid={taskRecords[0].childid} childrecord={taskRecords[0].childrecord} parentid={taskRecords[0].parentid} status={taskRecords[0].status} date={taskRecords[0].date} asms={taskRecords[0].asms} handle={taskRecords[0].handle} future={taskRecords[0].future} checkForRecords={taskRecords[0].checkForRecords} setCheckForRecords={taskRecords[0].setCheckForRecords} />
-    //                 <div>{taskRecords.slice(1).map(({ childid, childrecord, parentid, date, asms, handle, future }) => (<TaskRecordStatusByColourLong alertCtx={alertCtx} childid={childid} childrecord={childrecord} parentid={parentid} status={'ARCHIVE'} date={date} asms={asms} handle={handle} future={future} checkForRecords={checkForRecords} setCheckForRecords={setCheckForRecords} />))}</div>
-    //             </div>
-    //         )
-    //     }
-    // }
+    if (taskRecords.length > 0) {
+        if (taskRecords[0].status === 'DONE') {
+            return (
+                <div>
+                    <TaskRecordStatusByColourLong alertCtx={alertCtx} childid={taskRecords[0].childid} childrecord={taskRecords[0].childrecord} parentid={taskRecords[0].parentid} status={taskRecords[0].status} date={taskRecords[0].date} asms={taskRecords[0].asms} handle={taskRecords[0].handle} future={taskRecords[0].future} checkForRecords={taskRecords[0].checkForRecords} setCheckForRecords={taskRecords[0].setCheckForRecords} />
+                    <div>{taskRecords.slice(1).map(({ childid, childrecord, parentid, date, asms, handle, future }) => (<TaskRecordStatusByColourLong alertCtx={alertCtx} childid={childid} childrecord={childrecord} parentid={parentid} status={'ARCHIVE'} date={date} asms={asms} handle={handle} future={future} checkForRecords={checkForRecords} setCheckForRecords={setCheckForRecords} />))}</div>
+                </div>
+            )
+        }
+    }
 
     return (
         <div>
             <div className='Verdana'>
-                {taskRecords.map(({ childid, childrecord, parentid, status, date, asms, handle, future }) => (editableTaskRecord(alertCtx, childid, childrecord, parentid, status, date, asms, handle, future, checkForRecords, setCheckForRecords)))}
+                {taskRecords.map(({ childid, childrecord, parentid, status, date, asms, handle, future }) => (editableTaskRecord(childid, childrecord, parentid, status, date, asms, handle, checkForRecords, setCheckForRecords)))}
             </div>
+            
+            
+            
             <div>
                 {/* <MdAddCircleOutline style={{ color: '#D5441C', fontSize: '20px' }} /> */}
                 <Tooltip title='Insert Step Entry' placement="top-end"><button style={{ height: '20px', width: '20px', padding: 0, border: 'none', borderRadius: '3px', backgroundColor: 'white', outline: 'none', cursor: 'pointer' }} type='button' onClick={toggleAccordion}><MdAddCircleOutline style={{ color: 'D5441C', display: 'block', margin: 'auto', fontSize: '20px' }} /></button></Tooltip>
