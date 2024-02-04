4bee5f883bf7582963b0dcee2b65e9e623c17fe9
paulbsr <paul.bester@outlook.com>
2024-02-04 20:34:42 +0000

    Frontpage updates

--- src/MyCV/MyCV.jsx
+++ src/MyCV/MyCV.jsx
@@ -9,14 +9,25 @@ import TUS from './TUS.png'
 import LYIT from './LYIT.png'
 import DCU from './DCU.png'
 import MyCVEmployers from './MyCVEmployers';
+import { GiAchievement } from "react-icons/gi";
 
 
 export default function MyCV(props) {
   const [checkForRecords, setCheckForRecords] = useState(true);
-  const [isExpanded, setExpanded] = useState(false);
+  const [isExpandedSummary, setExpandedSummary] = useState(false);
+  const [isExpandedExpertise, setExpandedExpertise] = useState(false);
+  const [isExpandedHighlights, setExpandedHighlights] = useState(false);
   const [mycvdata, setMycvdata] = useState([]);
+  const [mycvcareersummary, setMycvcareersummary] = useState([]);
+  const [mycvcareerexpertise, setMycvcareerexpertise] = useState([]);
+  const [mycvcareerhighlights, setMycvcareerhighlights] = useState([]);
+
   const [error, setError] = useState(null);
-  const toggleAccordion = () => { setExpanded(!isExpanded); };
+  const toggleAccordionSummary = () => { setExpandedSummary(!isExpandedSummary); };
+  const toggleAccordionExpertise = () => { setExpandedExpertise(!isExpandedExpertise); };
+  const toggleAccordionHighlights = () => { setExpandedHighlights(!isExpandedHighlights); };
+
+
 
   // const handleLinkClick = (howtoId) => {
   //   setHowtoIdd(howtoId);
@@ -32,6 +43,44 @@ export default function MyCV(props) {
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+      });
+  }, [checkForRecords]);
+
+
+  useEffect(() => {
+    axios(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/mycv/summary`)
+      .then((response) => {
+        const mycvcareersummaryIB = response.data;
+        setMycvcareersummary(mycvcareersummaryIB);
+      })
+      .catch((error) => {
+        console.error("Error fetching mycvcareersummary data:", error);
+      });
+  }, [checkForRecords]);
+
+
+  useEffect(() => {
+    axios(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/mycv/expertise`)
+      .then((response) => {
+        const mycvcareerexpertiseIB = response.data;
+        mycvcareerexpertiseIB.sort((a, b) => b.expertise - a.expertise);
+        setMycvcareerexpertise(mycvcareerexpertiseIB);
+      })
+      .catch((error) => {
+        console.error("Error fetching mycvcareerexpertise data:", error);
+      });
+  }, [checkForRecords]);
+
+
+  useEffect(() => {
+    axios(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/mycv/highlights`)
+      .then((response) => {
+        const mycvcareerhighlightsIB = response.data;
+        mycvcareerhighlightsIB.sort((a, b) => b.highlight - a.highlight);
+        setMycvcareerhighlights(mycvcareerhighlightsIB);
+      })
+      .catch((error) => {
+        console.error("Error fetching mycvcareerhighlights data:", error);
       });
   }, [checkForRecords]);
 
@@ -77,60 +126,9 @@ export default function MyCV(props) {
           </table>
         </div>
         <div>&nbsp;</div>
-
       </div>
     );
   };
-
-
-  // const InnerTableCentre = () => {
-  //   return (
-  //     <div>
-  //       <table>
-  //         <div>
-  //           <table style={{ width: '1200px' }}>
-  //             <thead>
-  //               <tr className="TableCV">
-  //                 <th>Curriculum Vitae of Paul Bester</th>
-  //               </tr>
-                
-  //               <tr>
-  //                 <th className="CV-Font-Calibri-Large-Italic-PG">Professional Summary: 24 years in US-based multi-national IT sector. Experienced in innovation-driven and commerce-driven disruptive "Startup/Transition" activities. Hyper-efficient, detail orientated leader, manager and organiser of technical teams with responsibility in the Network/Communications stack. Technically proficient in Routing & Switching, WAN-layer transport, Data Center operations, Contact Centers and Collaboration technologies. Strong PgM/PM, customer-facing and vendor-management with Overseas/Expat Assignment experience.</th>
-  //               </tr>
-                
-  //               <tr>
-  //                 <th className="CV-Font-Calibri-Large-Italic-PG">Areas of Expertise</th>
-  //               </tr>
-                
-  //               <tr>
-  //                 <th className="CV-Font-Calibri-Large-Italic-PG">Career Highlights</th>
-  //               </tr>
-
-  //               <tr>
-  //                 <th className="CV-Font-Calibri-Large-Italic-PG">Testimonials</th>
-  //               </tr>
-
-  //             </thead>
-
-  //             {mycvdata && mycvdata.map((emp) => (
-  //               <tbody>
-  //                 {
-  //                   <tr>
-  //                     <td className="CV-Font-Calibri-Large-Italic-PG">
-  //                       {<MyCVEmployers mycvdata={mycvdata} key={emp.employer_id} employer_name={emp.employer_name} employer_start={emp.employer_start} employer_end={emp.employer_end} employer_id={emp.employer_id} employer_desc={emp.employer_desc} checkForRecords={checkForRecords} setCheckForRecords={setCheckForRecords} />}
-  //                     </td>
-  //                   </tr>
-  //                 }
-  //               </tbody>
-  //             )
-  //             )
-  //             }
-  //           </table>
-  //         </div>
-  //       </table>
-  //     </div>
-  //   );
-  // };
 
 
   const InnerTableCentre = () => {
@@ -138,29 +136,64 @@ export default function MyCV(props) {
       <div>
         <table style={{ width: '1200px' }}>
           <thead>
+            
             <tr className="TableCV">
               <th>Curriculum Vitae of Paul Bester</th>
             </tr>
-            
-            <tr>
-              <th className="CV-Font-Calibri-Large-Italic-PG">Professional Summary
-              {/* 24 years in US-based multi-national IT sector. Experienced in innovation-driven and commerce-driven disruptive "Startup/Transition" activities. Hyper-efficient, detail-orientated leader, manager, and organizer of technical teams with responsibility in the Network/Communications stack. Technically proficient in Routing & Switching, WAN-layer transport, Data Center operations, Contact Centers, and Collaboration technologies. Strong PgM/PM, customer-facing, and vendor-management with Overseas/Expat Assignment experience. */}
+
+            <tr className="Font-Calibri-Medium-MyCV">
+              <th >
+                This web presence was developed by me personally. You are welcome to review the <a href={'https://github.com/paulbsr/besterdev-ui'} target="_blank">front-end UI</a> and the <a href={'https://github.com/paulbsr/besterdev-api'} target="_blank">back-end API</a> repos on Github. The Tech Stack is JavaScript and NodeJS for the FE/UI hosted on AWS Amplify. The back-end API/MicroServices are developed in Java using the SpringBoot Framework hosted on Heroku alongisde the PostgreSQL database. Identity and Authorization (IAM) is porvided by Google's Firebase. I use Git for VCS and Maven for Builds. Swagger is available by clicking on the icon above.
               </th>
             </tr>
             
-            <tr>
-              <th className="CV-Font-Calibri-Large-Italic-PG">Areas of Expertise</th>
-            </tr>
             
-            <tr>
-              <th className="CV-Font-Calibri-Large-Italic-PG">Career Highlights</th>
-            </tr>
-  
+            <div className="CV-Font-Calibri-Large-Italic-PG" onClick={toggleAccordionSummary}><b>Professional Summary:</b>
+            {isExpandedSummary && (
+              <div className="mycvhover">
+                {mycvcareersummary && mycvcareersummary.map((summary) =>
+                (
+                  <tr key={summary.id}>{summary.career_summary}</tr>
+                )
+                )
+                }
+              </div>)}
+            </div>
+            <div>&nbsp;</div>
+
+
+            <div className="CV-Font-Calibri-Large-Italic-PG" onClick={toggleAccordionExpertise}><b>Areas of Expertise:</b>
+             {isExpandedExpertise && (
+               <div >
+                 {mycvcareerexpertise && mycvcareerexpertise.map((expertise) =>
+                 (
+                  <tr className="mycvhover" key={expertise.id}> <GiAchievement style={{ color: '#169247', display: 'round', margin: 'auto', fontSize: '20px' }} />{expertise.expertise_desc}</tr>
+                 )
+                 )
+                 }
+               </div>)}
+             </div>
+             <div>&nbsp;</div>
+
+
+             <div className="CV-Font-Calibri-Large-Italic-PG" onClick={toggleAccordionHighlights}><b>Career Highlights:</b>
+             {isExpandedHighlights && (
+               <div >
+                 {mycvcareerhighlights && mycvcareerhighlights.map((highlight) =>
+                 (
+                  <tr key={highlight.id}> <GiAchievement style={{ color: '#169247', display: 'round', margin: 'auto', fontSize: '19px' }} />{highlight.highlight_desc}</tr>
+                 )
+                 )
+                 }
+               </div>)}
+             </div>
+             <div>&nbsp;</div>
+
             <tr>
               <th className="CV-Font-Calibri-Large-Italic-PG">Testimonials</th>
             </tr>
           </thead>
-  
+
           <tbody>
             {mycvdata && mycvdata.map((emp) => (
               <tr key={emp.employer_id}>
@@ -183,7 +216,7 @@ export default function MyCV(props) {
       </div>
     );
   };
-  
+
 
 
   const InnerTableRight = () => {
@@ -200,7 +233,7 @@ export default function MyCV(props) {
             <div>LetterkennyIT (LYIT), 2019</div>
             <div className='Font-Spacer-White'>Make this spacer white</div>
             <img src={DCU} width="240" height="300" />
-            <div>BSc. Information Technology</div>
+            <div>BSc. Information Technology & Computer Science</div>
             <div>Dublin City University (DCU), 2007</div>
           </tbody>
         </table>
