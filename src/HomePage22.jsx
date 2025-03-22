fa80735ecd889cf65e4522400925b9f64a0aeeb1
paulbsr <paul.bester@outlook.com>
2025-03-22 14:04:23 +0000

    More

--- src/HomePage22.jsx
+++ src/HomePage22.jsx
@@ -36,8 +36,8 @@ export default function HomePage22(props) {
   }, [props.checkForRecords]);
 
   useEffect(() => {
-    // axios('https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/alphabet/random')
-    axios('http://localhost:8000/api/v1/cyclopedia/alphabet/random')
+    axios('https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/alphabet/random')
+    // axios('http://localhost:8000/api/v1/cyclopedia/alphabet/random')
       // axios('https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/cyclopedia/random40')
       .then((response) => {
         const fourtyRandomRecordsAPI = response.data;
