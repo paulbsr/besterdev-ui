9769aa8befa39d5f1f5c94a369baf1990fa93658
paulbsr <paul.bester@outlook.com>
2025-04-07 16:09:25 +0100

    Trying to fix

--- src/websockets/WebSocketEvent.jsx
+++ src/websockets/WebSocketEvent.jsx
@@ -1,40 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import SockJS from 'sockjs-client';
-import { Client } from '@stomp/stompjs';
-import '../Fonts.css';
-import { TbBrandSocketIo } from "react-icons/tb";
+// import React, { useEffect, useState } from 'react';
+// import SockJS from 'sockjs-client';
+// import { Client } from '@stomp/stompjs';
+// import '../Fonts.css';
 
-const WebSocketEvent = () => {
-  const [params, setParams] = useState("SocketServer nothing published");
+// const WebSocketEvent = () => {
+//   const [params, setParams] = useState("SocketServer nothing published");
 
-  useEffect(() => {
-    const socket = new SockJS('https://besterdev-api-13a0246c9cf2.herokuapp.com/ws');
-    const client = new Client({
-      webSocketFactory: () => socket,
-      debug: (str) => console.log(str),
-      onConnect: () => {
-        console.log('Connected to WebSocket');
-        client.subscribe('/topic/websocketmessage', (message) => {
-          // const body = JSON.parse(message.body);
-          const body = (message.body);
-          setParams(body);
-        });
-      },
-    });
+//   useEffect(() => {
+//     const socket = new SockJS('https://besterdev-api-13a0246c9cf2.herokuapp.com/ws');
+//     const client = new Client({
+//       webSocketFactory: () => socket,
+//       debug: (str) => console.log(str),
+//       onConnect: () => {
+//         console.log('Connected to WebSocket');
+//         client.subscribe('/topic/websocketmessage', (message) => {
+//           // const body = JSON.parse(message.body);
+//           const body = (message.body);
+//           setParams(body);
+//         });
+//       },
+//     });
 
-    client.activate();
+//     client.activate();
 
-    return () => {
-      client.deactivate();
-    };
-  }, []);
+//     return () => {
+//       client.deactivate();
+//     };
+//   }, []);
 
-  return (
-    <div>
-      <td style={{ fontFamily: "Segoe UI", fontSize: "small", color: "rgb(148, 196, 245)" }}>Latest WebSocket Event: <b>{params}</b></td>
+//   return (
+//     <div>
+//       <td style={{ fontFamily: "Segoe UI", fontSize: "small", color: "rgb(148, 196, 245)" }}>Latest WebSocket Event: <b>{params}</b></td>
       
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default WebSocketEvent;
+// export default WebSocketEvent;
