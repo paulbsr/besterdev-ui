a52690db609b534cd464ff2bdc18c2f3225bb117
paulbsr <paul.bester@outlook.com>
2025-06-15 00:03:33 +0100

    more

--- src/oauth2.0/BearerToken.jsx
+++ src/oauth2.0/BearerToken.jsx
@@ -1,32 +1,79 @@
 import { SiJsonwebtokens } from 'react-icons/si';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function BearerToken() {
-  const [bearertoken, setBearertoken] = useState([]);
+  const [bearertoken, setBearertoken] = useState(null);
 
   const handleClick = async () => {
     try {
-      const response = await axios.get(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/auth/token?username=admin&password=password`);
+      const username = 'admin';
+      const password = 'password';
+      const credentials = btoa(`${username}:${password}`); // base64 encode
+
+      const response = await axios.get(
+        'https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/auth/token',
+        // 'http://localhost:8000/api/v1/auth/token',
+        {
+          headers: {
+            Authorization: `Basic ${credentials}`,
+          },
+        }
+      );
+
       setBearertoken(response.data);
-      console.log("Bearer Token received:", bearertoken);
+      console.log('Bearer Token received:', response.data);
     } catch (error) {
       console.error('Bearer Token error:', error);
-
     }
   };
 
-
   return (
-    <td className='Font-Verdana-Small-Postgres' onClick={handleClick}>
+    <td className="Font-Verdana-Small-Postgres" onClick={handleClick}>
       &nbsp;&nbsp;
       <SiJsonwebtokens
         title="Fetch a Bearer Token"
         style={{ color: '#4D4D4D', fontSize: '18px', cursor: 'pointer' }}
-      />  Bearer Token <div>{bearertoken.BearerToken}</div>
+      />
+      Bearer Token:S
+      <div>{bearertoken?.BearerToken}</div>
     </td>
   );
 }
 
 export default BearerToken;
+
+
+
+// import { SiJsonwebtokens } from 'react-icons/si';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios'
+
+// function BearerToken() {
+//   const [bearertoken, setBearertoken] = useState([]);
+
+//   const handleClick = async () => {
+//     try {
+//       const response = await axios.get(`https://besterdev-api-13a0246c9cf2.herokuapp.com/api/v1/auth/token?username=admin&password=password`);
+//       setBearertoken(response.data);
+//       console.log("Bearer Token received:", bearertoken);
+//     } catch (error) {
+//       console.error('Bearer Token error:', error);
+
+//     }
+//   };
+
+
+//   return (
+//     <td className='Font-Verdana-Small-Postgres' onClick={handleClick}>
+//       &nbsp;&nbsp;
+//       <SiJsonwebtokens
+//         title="Fetch a Bearer Token"
+//         style={{ color: '#4D4D4D', fontSize: '18px', cursor: 'pointer' }}
+//       />  Bearer Token <div>{bearertoken.BearerToken}</div>
+//     </td>
+//   );
+// }
+
+// export default BearerToken;
 
