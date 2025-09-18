8052020530a212f62b7571f7acfb95ba30e1ddd1
paulbsr <paul.bester@outlook.com>
2025-09-18 22:45:23 +0100

    remove reat-tooltip

--- src/dutchlanguage/DutchLanguageHomePage.jsx
+++ src/dutchlanguage/DutchLanguageHomePage.jsx
@@ -10,12 +10,13 @@ import DutchLanguageTicker from "./DutchLanguageTicker";
 import AskAI from '../openai/AskAI';
 import DutchLanguageWoordenschat from "./DutchLanguageWoordenschat";
 import DutchLanguageSentenceTrainerModal from "./DutchLanguageSentenceTrainerModal";
-import 'react-tooltip/dist/react-tooltip.css';
-import { Tooltip } from 'react-tooltip';
+// import 'react-tooltip/dist/react-tooltip.css';
+// import { Tooltip } from 'react-tooltip';
 // import DutchLanguageSentenceTrainer from "./DutchLanguageSentenceTrainer";
 import { FaRadio } from "react-icons/fa6";
 import { RiWebcamFill } from "react-icons/ri";
-import { GrWebcam } from "react-icons/gr";
+import { PiExamFill } from "react-icons/pi";
+// import { GrWebcam } from "react-icons/gr";
 
 export default function DutchLanguageHomePage() {
   return (
@@ -37,15 +38,17 @@ export default function DutchLanguageHomePage() {
 
         {/* CENTRE COLUMN */}
         <div className="main">
-                <Tooltip id="insert" place="top" />
+                {/* <Tooltip id="insert" place="top" /> */}
           <img src={Image} alt="Amsterdam" style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)", borderRadius: "8px", }} />
           <div>&nbsp;&nbsp;</div>
           <div><DutchLanguageTicker /></div>
           <div>&nbsp;&nbsp;</div>
           <div><AskAI /></div>
           <div style={{ marginTop: "5px", marginLeft: "20px" }}>
-            <a href="https://www.nporadio1.nl/live" target="_blank" rel="noopener noreferrer" data-tooltip-id="insert" data-tooltip-content="NPO Radio 1" style={{ cursor: "pointer", color: "#c0c0c0", fontSize: "25px" }}><FaRadio /></a>
-            <a href="https://www.skylinewebcams.com/en/webcam/netherlands/north-holland/amsterdam/amsterdam-dam-square.html" target="_blank" rel="noopener noreferrer" data-tooltip-id="insert" data-tooltip-content="Damrak Square Webcam" style={{ cursor: "pointer", color: "#c0c0c0", fontSize: "25px", marginLeft: "20px" }}><RiWebcamFill/></a>
+            <a href="https://www.nporadio1.nl/live" target="_blank" rel="noopener noreferrer" style={{ cursor: "pointer", color: "#c0c0c0", fontSize: "25px" }}><FaRadio /></a>
+            <a href="https://www.skylinewebcams.com/en/webcam/netherlands/north-holland/amsterdam/amsterdam-dam-square.html" target="_blank" rel="noopener noreferrer" style={{ cursor: "pointer", color: "#c0c0c0", fontSize: "25px", marginLeft: "20px" }}><RiWebcamFill/></a>
+            <a href="https://oefenexamensnt2.nl/facet-openbaar-portaal/welkom" target="_blank" rel="noopener noreferrer" style={{ cursor: "pointer", color: "#c0c0c0", fontSize: "24px", marginLeft: "20px" }}><PiExamFill/></a>
+            <a href="https://www.staatsexamensnt2.nl/" target="_blank" rel="noopener noreferrer" style={{ cursor: "pointer", color: "#c0c0c0", fontSize: "24px", marginLeft: "20px" }}><PiExamFill/></a>
           </div>
           <div><DutchLanguageSentenceCompletion /></div>
           <div>&nbsp;&nbsp;</div>
